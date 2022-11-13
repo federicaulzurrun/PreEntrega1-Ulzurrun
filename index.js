9f8c845a9256eba6aed95d04abc1d99e3db074c4
Federica Ulzurrun <fzulzurrun@gmail.com>
2022-11-13 16:50:15 -0300

    ultimo detalle

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function compra(){
         respuesta =
         prompt('Responde si la siguiente lista muestra los productos que quieres comprar: \nBikini Palm \nBikini Shell \nBikini Tie Dye \nResponde: \nsi : Si es correcta \nno : No son los productos que selecioné')
         
-    } while(respuesta ==='si' && respuesta ==='no' ){
+    } while (respuesta === 'si' && respuesta === 'no'){
         if (respuesta === 'si'){
-            respuesta = totalCompra;
             alert('Tu total a pagar es de $13200 \nMuchas Gracias por tu compra en Zöld!')
         } else if (respuesta === 'no'){
-            respuesta = errorCompra;
             alert('Disculpe las molestias')
+        }else if (respuesta != 'si', 'no'){
+            alert('Ingrese una respuesta válida')
         } else{
             alert('Que tenga un buen dia!')
         }
